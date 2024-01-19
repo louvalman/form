1c3bfec4979a7ea994a771ccca2f81f4b12c8143
louvalman <louisdyrhauge@hotmail.com>
2024-01-19 22:24:48 +0100

    Add tippy tooltip next to H1

--- form.js
+++ form.js
@@ -1,7 +1,9 @@
+const root = document.documentElement;
+
+// Theme functionality
 const themeSwitch = document.querySelector('body > header > span > i');
 
 function setTheme() {
-  const root = document.documentElement;
   const newTheme = root.className === 'dark' ? 'light' : 'dark';
   root.className = newTheme;
   themeSwitch.classList.toggle('iconoir-light-bulb-on');
@@ -9,3 +11,17 @@ function setTheme() {
 }
 
 document.querySelector('.theme-toggle').addEventListener('click', setTheme);
+
+// Tooltip functionality using TippyJS
+
+tippy('#info', {
+  content:
+    'This is a form template created to showcase various client-side validation methods using semantic HTML, CSS and vanilla JS. <br><br>Check out my <strong><a href="https://www.github.com/louvalman" class="github-link">Github</a></strong> for more usable templates!',
+  arrow: true,
+  trigger: 'click',
+  arrowType: 'round',
+  animateFill: false,
+  animation: 'scale',
+  interactive: true,
+  theme: 'light',
+});
