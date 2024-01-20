33940d8d530eabbbf18ed16784bebd520896a036
louvalman <louisdyrhauge@hotmail.com>
2024-01-21 00:51:29 +0100

    Create new input fields with floating labels and tooltip UI adjustments

--- form.js
+++ form.js
@@ -13,15 +13,23 @@ function setTheme() {
 document.querySelector('.theme-toggle').addEventListener('click', setTheme);
 
 // Tooltip functionality using TippyJS
-
 tippy('#info', {
-  content: 'This is a form template created to showcase various client-side validation methods using semantic HTML, CSS and vanilla JS. <br><br>Check out my <strong><a href="https://www.github.com/louvalman" class="github-link">Github</a></strong> for more usable templates!',
+  content:
+    'This is a form template created to showcase various client-side validation methods using semantic HTML, CSS and vanilla JS. <br><br>Check out my <strong><a href="https://www.github.com/louvalman" class="github-link">Github</a></strong> for more usable templates!',
   arrow: true,
   trigger: 'click',
   arrowType: 'round',
   animateFill: false,
   animation: 'shift-away',
   interactive: true,
-  theme: 'light',
-  placement: 'left'
+  placement: 'left',
+});
+
+// Clear input
+document.addEventListener('click', function (event) {
+  let clearButton = event.target.closest('.clear');
+  if (clearButton) {
+    let inputId = clearButton.getAttribute('data-input-id');
+    document.getElementById(inputId).value = '';
+  }
 });
