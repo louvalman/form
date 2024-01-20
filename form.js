5e4fcc6fccae38bf6913e5de9b1eda2a8db4a07c
louval <94385943+louvalman@users.noreply.github.com>
2024-01-20 03:16:12 +0100

    Create fun svg background dynamic with theme change and header

--- form.js
+++ form.js
@@ -15,13 +15,13 @@ document.querySelector('.theme-toggle').addEventListener('click', setTheme);
 // Tooltip functionality using TippyJS
 
 tippy('#info', {
-  content:
-    'This is a form template created to showcase various client-side validation methods using semantic HTML, CSS and vanilla JS. <br><br>Check out my <strong><a href="https://www.github.com/louvalman" class="github-link">Github</a></strong> for more usable templates!',
+  content: 'This is a form template created to showcase various client-side validation methods using semantic HTML, CSS and vanilla JS. <br><br>Check out my <strong><a href="https://www.github.com/louvalman" class="github-link">Github</a></strong> for more usable templates!',
   arrow: true,
   trigger: 'click',
   arrowType: 'round',
   animateFill: false,
-  animation: 'scale',
+  animation: 'shift-away',
   interactive: true,
   theme: 'light',
+  placement: 'left'
 });
